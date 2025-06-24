e86aeef7d670819d16ee4f5ac7cddb79d43f39c4
Don Park <donp@donp.org>
2025-06-24 09:00:28 +0700

    contract progress

--- ethereum/contracts/UsdCoin.sol
+++ ethereum/contracts/UsdCoin.sol
@@ -1,13 +1,20 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import 'openzeppelin-contracts-5.3.0/contracts/token/ERC20/ERC20.sol';
+import "openzeppelin-contracts-5.3.0/contracts/token/ERC20/ERC20.sol";
 
-    
 contract UsdCoin is ERC20 {
-  constructor() ERC20("Eggplant", "EGGP") {}
+    address public owner;
+    constructor() ERC20("Eggplant", "EGGP") {
+        owner = msg.sender;
+    }
 
-  function mint(address to, uint256 amount) public {
-    _mint(to, amount);
-  }
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "only owner can call this");
+        _;
+    }
 }
