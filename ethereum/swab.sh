f0ff585fd410dab68e422a3de9226a393ed2278e
Don Park <donp@donp.org>
2025-07-24 12:02:20 +0700

    show balances

--- ethereum/swab.sh
+++ ethereum/swab.sh
@@ -21,12 +21,12 @@ echo USDONC for HAT2 = $COIN
 reserves() {
 echo pool0 ${POOL0} reserves
 eth contract:call uniswap-v2-pair@${POOL0} 'getReserves()' | grep reserve
-eth contract:call uniswap-v2-pair@${POOL0} 'token0()'
-eth contract:call uniswap-v2-pair@${POOL0} 'token1()'
+echo token0 `eth contract:call uniswap-v2-pair@${POOL0} 'token0()'`
+echo token1 `eth contract:call uniswap-v2-pair@${POOL0} 'token1()'`
 echo pool1 ${POOL1} reserves
 eth contract:call uniswap-v2-pair@${POOL1} 'getReserves()' | grep reserve
-eth contract:call uniswap-v2-pair@${POOL1} 'token0()'
-eth contract:call uniswap-v2-pair@${POOL1} 'token1()'
+echo token0 `eth contract:call uniswap-v2-pair@${POOL1} 'token0()'`
+echo token1 `eth contract:call uniswap-v2-pair@${POOL1} 'token1()'`
 
 COIN=`eth contract:call erc20@usdona 'balanceOf("'${POOL0}'")'`
 echo USDONA for pool0 = $COIN
@@ -38,12 +38,12 @@ COIN=`eth contract:call erc20@usdonc 'balanceOf("'${POOL1}'")'`
 echo USDONC for pool1 = $COIN
 }
 
-#balances
 STARTC=`eth contract:call erc20@usdonc 'balanceOf("'${HAT2}'")'`
+balances
 # SWAB!
 echo swab\(${AY_IN}, ${POOL0}, ${POOL1}\)
 eth contract:send --pk hat2 uniswab@${SWAB} 'swab('${AY_IN}', "'${POOL0}'", "'${POOL1}'")'
-#balances
+balances
 reserves
 ENDC=`eth contract:call erc20@usdonc 'balanceOf("'${HAT2}'")'`
 echo ay_in ${AY_IN} profit `calc "${ENDC} - ${STARTC}"`
